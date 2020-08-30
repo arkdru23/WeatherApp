c669b1644255ea5f0a7fb7d933e5610af064c3a7
Arkadiusz Drukała <arkadiusz.drukala@gmail.com>
2020-08-30 14:35:21 +0200

    Updates

---
+++ precache-manifest.ba61d0f32bd72f7d54b7f764aee0186f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c44ba6d37bf2394a3ebb2314e007f166",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "781f89acadbd53a9e446",
+    "url": "/WeatherApp/static/css/main.41151b41.chunk.css"
+  },
+  {
+    "revision": "c19acd0832b2880df62e",
+    "url": "/WeatherApp/static/js/2.14e2fc7a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/WeatherApp/static/js/2.14e2fc7a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "781f89acadbd53a9e446",
+    "url": "/WeatherApp/static/js/main.20509c58.chunk.js"
+  },
+  {
+    "revision": "8be95e4115b1d44862e2",
+    "url": "/WeatherApp/static/js/runtime-main.ded04fe5.js"
+  }
+]);
